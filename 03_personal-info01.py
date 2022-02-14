8f72d0949cc235a9039c9e87fa94ab3a9bdb1a6f
CESDELBRA <98853133+CESDELBRA@users.noreply.github.com>
2022-02-13 19:59:31 -0500

    Add files via upload

--- 03_personal-info01.py
+++ 03_personal-info01.py
@@ -11,3 +11,13 @@ direccion=input("Ingrese su direccion: ")
 edad=int(input("Ingresa tu edad: "))
 space=""
 print("Su nombre es",nombre,space,apellido,"vive en la calle",direccion,space,"y suedad es de ",edad,"anos")
+if edad<13:
+    print(nombre, "Eres un nino")
+elif edad>=13 and edad<18:
+    print(nombre, "Eres un adolecente")
+elif edad>=18 and edad<33:
+    print(nombre, "Eres un joven")
+elif edad>=33 and edad<65:
+    print(nombre, "Eres un adulto") 
+else:
+    print(nombre, "Eres un adulto mayor")         
